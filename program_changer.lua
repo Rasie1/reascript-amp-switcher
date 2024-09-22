eddd0c24708d4df48ba42e5e23d3c2b8b8ef7752
Rasie1 <rasielll@gmail.com>
2024-09-22 13:49:33 +0200

    Update

--- program_changer.lua
+++ program_changer.lua
@@ -12,13 +12,13 @@ reaper.SetExtState("ProgramChanger", "KeyDownTime", reaper.time_precise(), true)
 local presetString = reaper.GetExtState("ProgramChanger", "Preset")
 local currentPreset = tonumber(presetString)
 if currentPreset == nil then
-    currentPreset = 0
+    currentPreset = 1
 end
 
 local otherPresetString = reaper.GetExtState("ProgramChanger", "OtherPreset")
 local otherPreset = tonumber(otherPresetString)
 if otherPreset == nil then
-    otherPreset = 1
+    otherPreset = 2
 end
 
 -- swap them
@@ -28,6 +28,27 @@ local newPreset = currentPreset
 if newPreset == otherPreset then
     newPreset = (otherPreset + 1) % presetsNumber
 end
+
+-- skip 0 because it's a workaround for reset after playback
+if newPreset == 0 then
+    if otherPreset == 1 then
+        newPreset = 2
+    else
+        newPreset = 1
+    end
+    reaper.ShowMessageBox(0, "DEBUG", 0)
+end
+if otherPreset == 0 then
+    if newPreset == 1 then
+        otherPreset = 2
+    else
+        otherPreset = 1
+    end
+    reaper.ShowMessageBox(1, "DEBUG", 0)
+end
+
+-- reaper.ShowMessageBox(newPreset, "DEBUG", 0)
+
 reaper.SetExtState("ProgramChanger", "OtherPreset", newPreset, true)
 newPreset = otherPreset
 reaper.SetExtState("ProgramChanger", "Preset", newPreset, true)
--- program_changer_cache_c12.lua
+++ program_changer_cache_c12.lua
@@ -6,9 +6,6 @@ end
 is_new_value,filename,sectionID,cmdID,mode,resolution,value,contextstr = reaper.get_action_context()
 local newVelocity = 0
 if (currentPreset == 0) then
-    newVelocity = math.min(127, math.max(0, (140 - value - value // 4) // 2))
-    reaper.StuffMIDIMessage(0, 0xB8, 12, newVelocity)
-    reaper.SetExtState("MidiCache", "c14", newVelocity, true)
 elseif (currentPreset == 1) then
     local rooted = value^0.4 * 31
     newVelocity = math.floor(math.max(0,math.min(rooted - 100, 127)))
@@ -29,8 +26,9 @@ elseif (currentPreset == 4) then
     reaper.StuffMIDIMessage(0, 0xB8, 12, newVelocity)
     reaper.SetExtState("MidiCache", "c14", newVelocity, true)
 elseif (currentPreset == 5) then
-    newVelocity = math.min(127, math.max(0, 180 - value - value // 2))
-    reaper.StuffMIDIMessage(0, 0xB8, 13, newVelocity)
-    reaper.SetExtState("MidiCache", "c13", newVelocity, true)
+    newVelocity = math.min(127, math.max(0, (140 - value - value // 4) // 2))
+    reaper.StuffMIDIMessage(0, 0xB8, 12, newVelocity)
+    reaper.SetExtState("MidiCache", "c14", newVelocity, true)
 end
 
+-- old
